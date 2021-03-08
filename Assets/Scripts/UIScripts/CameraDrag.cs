07991401bba4b477c075cae74bc9a52cbe6182c3
MatthewGaby <pixelball1213@gmail.com>
2021-03-08 02:58:59 -0600

    Adjusted Camera to stay within boundaries regardless of size
    We can adjust these boundaries as we see fit

--- Assets/Scripts/UIScripts/CameraDrag.cs
+++ Assets/Scripts/UIScripts/CameraDrag.cs
@@ -34,14 +34,18 @@ public class CameraDrag : MonoBehaviour
         {
             float speed = dragSpeed * Time.fixedDeltaTime;
             camera.transform.position -= new Vector3(Input.GetAxis("Mouse X") * speed, Input.GetAxis("Mouse Y") * speed);
-            Vector3 newPos = camera.transform.position;
-            newPos.x = Mathf.Clamp(newPos.x, minCameraPos.x, maxCameraPos.x);
-            newPos.y = Mathf.Clamp(newPos.y, minCameraPos.y, maxCameraPos.y);
-            camera.transform.position = newPos;
+            
         }
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
         camera.transform.Translate(new Vector3(xAxis*Time.fixedDeltaTime, yAxis*Time.fixedDeltaTime, 0));
+
+        // Limits camera to certain position range
+        /*Vector3 newPos = camera.transform.position;
+        newPos.x = Mathf.Clamp(newPos.x, minCameraPos.x, maxCameraPos.x);
+        newPos.y = Mathf.Clamp(newPos.y, minCameraPos.y, maxCameraPos.y);
+        camera.transform.position = newPos; */
+        camera.transform.position = ClampCamera(camera.transform.position);
         if (Input.mouseScrollDelta.y > 0.5f && camera.orthographicSize > minZoom)
         {
             camera.orthographicSize -= 1;
@@ -50,5 +54,20 @@ public class CameraDrag : MonoBehaviour
         {
             camera.orthographicSize += 1;
         }
+    }
+
+    private Vector3 ClampCamera(Vector3 targetPosition)
+    {
+        float cameraHeight = camera.orthographicSize;
+        float cameraWidth = camera.orthographicSize * camera.aspect;
+        float minX = minCameraPos.x + cameraWidth;
+        float maxX = maxCameraPos.x - cameraWidth;
+        float minY = minCameraPos.y + cameraHeight;
+        float maxY = maxCameraPos.y - cameraHeight;
+
+        float newX = Mathf.Clamp(targetPosition.x, minX, maxX);
+        float newY = Mathf.Clamp(targetPosition.y, minY, maxY);
+
+        return new Vector3(newX, newY, targetPosition.z);
     }
 }
