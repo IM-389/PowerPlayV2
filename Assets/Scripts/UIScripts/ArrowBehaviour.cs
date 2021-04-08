fb36deadf720446ea945240f3536fdee846455f1
darlynn-best <70346167+darlynn-best@users.noreply.github.com>
2021-04-08 15:25:09 -0500

    Arrow is working but needs to be added to main scene.

--- Assets/Scripts/UIScripts/ArrowBehaviour.cs
+++ Assets/Scripts/UIScripts/ArrowBehaviour.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ArrowBehaviour : MonoBehaviour
 {
+    public GameObject target;
+    //public GameObject cam;
+    public float speed = 1;
+    float angle;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector3 targetPos = target.transform.position - cam.transform.position;
+        //Vector3 dis = targetPos - gameObject.transform.position;
+        Vector3 dis = target.transform.position - gameObject.transform.position;
+        angle = Mathf.Atan2(dis.y, dis.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, q, Time.deltaTime * speed);
     }
 }
