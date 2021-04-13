054ed4f93e430c142d81de5ebf271cdc11acd5d5
Matthew Sciarabba <70346575+Mjsciarabba@users.noreply.github.com>
2021-04-13 17:37:34 -0500

    Saved Volume Settings to PlayerPrefs
    -as title says
    -also consolidated slidervalue script into soundmanager

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
@@ -10,15 +11,24 @@ public class SoundManager : MonoBehaviour
     FMOD.Studio.Bus Master;
     [Header("Volume Buses")]
     public string masterBus;
-    float masterVolume = 1f;
+    float masterVolume;
 
     FMOD.Studio.Bus Music;
     public string musicBus;
-    float musicVolume = 0.5f;
+    float musicVolume;
+    
 
     FMOD.Studio.Bus SFX;
     public string sfxBus;
-    float sfxVolume = 0.5f;
+    float sfxVolume;
+
+    [Header("Volume Sliders")]
+    //public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    public Text musicSliderText;
+    public Text sfxSliderText;
 
     // These are the locations of the sound events
     [Header("Sound Locations")]
@@ -37,12 +47,19 @@ public class SoundManager : MonoBehaviour
         Master = RuntimeManager.GetBus(masterBus);
         Music = RuntimeManager.GetBus(musicBus);
         SFX = RuntimeManager.GetBus(sfxBus);
+
+        LoadSettings();
+
+        musicSlider.value = musicVolume * 100;
+        sfxSlider.value = sfxVolume * 100;
+        
+        SetSliderText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Master.setVolume(masterVolume);
+        //Master.setVolume(masterVolume);
         Music.setVolume(musicVolume);
         SFX.setVolume(sfxVolume);
     }
@@ -51,10 +68,13 @@ public class SoundManager : MonoBehaviour
     /// Handles the Master Volume for all sounds
     /// </summary>
     /// <param name="newVolume"></param>
-    public void MasterVolumeLevel (float newVolume)
-    {
-        masterVolume = newVolume / 100;
-    }
+    // public void MasterVolumeLevel (float newVolume)
+    // {
+    //     masterVolume = newVolume / 100;
+    //     PlayerPrefs.SetFloat("Master Volume", masterVolume);
+    //     PlayerPrefs.Save();
+    //     SetSliderText();
+    // }
 
     /// <summary>
     /// Handles the volume level of all music and backgrounds
@@ -63,6 +83,7 @@ public class SoundManager : MonoBehaviour
     public void MusicVolumeLevel (float newVolume)
     {
         musicVolume = newVolume / 100;
+        SetSliderText();
     }
 
     /// <summary>
@@ -72,6 +93,7 @@ public class SoundManager : MonoBehaviour
     public void SFXVolumeLevel (float newVolume)
     {
         sfxVolume = newVolume / 100;
+        SetSliderText();
     }
 
     /// <summary>
@@ -88,5 +110,45 @@ public class SoundManager : MonoBehaviour
     public void ButtonClick()
     {
         RuntimeManager.PlayOneShot(buttonPress);
+        PlayerPrefs.Save();
+    }
+    
+    void SetSliderText()
+    {
+        musicSliderText.text = "Music = " + (Mathf.Round(musicSlider.value)) + "%";
+        sfxSliderText.text = "SFX = " + (Mathf.Round(sfxSlider.value)) + "%";
+    }
+
+    public void LoadSettings()
+    {
+        if (PlayerPrefs.HasKey("Music Volume"))
+        {
+            musicVolume = PlayerPrefs.GetFloat("Music Volume");
+        }
+        else
+        {
+            musicVolume = 0.5f;
+        }
+
+        if (PlayerPrefs.HasKey("SFX Volume"))
+        {
+            sfxVolume = PlayerPrefs.GetFloat("SFX Volume");
+        }
+        else
+        {
+            sfxVolume = 0.5f;
+        }
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetFloat("Music Volume", musicVolume);
+        PlayerPrefs.SetFloat("SFX Volume", sfxVolume);
+
+    }
+
+    private void OnDisable()
+    {
+        SaveSettings();
     }
 }
