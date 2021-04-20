dfdcb5c2e9d8d3756b11a4eca4fe0fd9ec10f474
Doug Guzman <swiftshadow34@gmail.com>
2021-04-19 21:05:51 -0500

    Adds autoreset code to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 //nothing's here at the moment, obviously-I feel like we can probably put most of the gameObject scripts in here for ease of management?
 public class GameManager : MonoBehaviour
 {
-    public GameObject helpScreen;
+    public GameObject[] toEnable;
+
+    public GameObject[] toDisable;
+
+    private void Awake()
+    {
+        foreach (var item in toEnable)
+        {
+            item.SetActive(true);
+        }
+
+        foreach (var item in toDisable)
+        {
+            item.SetActive(false);
+        }
+    }
 
 }
