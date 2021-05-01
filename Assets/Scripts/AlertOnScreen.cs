1ef0557c801a4c3cf0b6f44770e96a772a528d3e
MatthewGaby <pixelball1213@gmail.com>
2021-05-01 15:43:18 -0500

    Fixed smart alert layering/staying on screen to a smaller box

--- Assets/Scripts/AlertOnScreen.cs
+++ Assets/Scripts/AlertOnScreen.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class AlertOnScreen : MonoBehaviour
 {
     Vector3 startPos;
-
+    public float minStayOnX = 0.1f;
+    public float maxStayOnX = 0.9f;
+    public float minStayOnY = 0.1f;
+    public float maxStayOnY = 0.9f;
     private void Start()
     {
         startPos = transform.position;
@@ -15,8 +18,8 @@ public class AlertOnScreen : MonoBehaviour
     {
         transform.position = startPos;
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-        pos.x = Mathf.Clamp(pos.x , 0.1f, 0.9f);
-        pos.y = Mathf.Clamp(pos.y , 0.1f, 0.9f);
+        pos.x = Mathf.Clamp(pos.x , minStayOnX, maxStayOnX);
+        pos.y = Mathf.Clamp(pos.y , minStayOnY, maxStayOnY);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
